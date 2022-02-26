a7ca8225f689b0aa5040e96520af5919acfb3917
Austin White <austinlldb@gmail.com>
2022-02-25 18:49:08 -0700

    handle index page

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let listener: TcpListener = 
@@ -7,6 +10,29 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection extablished!");
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        // some other request
     }
 }
